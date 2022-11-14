55e119cd78dd6b0e53d9bed5842d8bb3a417ee12
Harold Jean <haroldjean38@yahoo.com>
2022-11-14 15:38:50 -0500

    fetched allDogs and rendered dogs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// Imports
 import { getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
+// Elements
 const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
 
 // fetch all dogs
+window.addEventListener('load', async () => {
+    await getDogs();
+});
 // render and append all dog cards to the container
+renderDogCard();
