9183dc5022ebd722a3c86671735134ca532f665c
Harold Jean <haroldjean38@yahoo.com>
2022-11-14 15:14:13 -0500

    exported dog and dogs function

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ import { renderDogCard } from './render-utils.js';
 const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
+
 // fetch all dogs
 // render and append all dog cards to the container
